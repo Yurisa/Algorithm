c1ed37a235d40d5c5e6d6d8e5b9c53f98897e545
Yurisa <779509411@qq.com>
2018-03-28 20:58:36 +0800

    冒泡排序

--- js基础算法/sort.js
+++ js基础算法/sort.js
@@ -1,7 +1,7 @@
 /**
  * 选择排序
+ * @param {*} arr 
  */
-
 function selectionSort(arr){
     for(let i = 0; i < arr.length; i++){
         minIndex = i;
@@ -11,5 +11,19 @@ function selectionSort(arr){
             }
         }
         [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+}
+
+/**
+ * 冒泡排序
+ * @param {*} arr 
+ */
+function bubbleSort(arr){
+    for(let i = 0; i < arr.length - 1;i++){
+        for(let j = 0; j < arr.length - i - 1;j++){
+            if(arr[j] >= arr[j+1]){
+                [arr[j+1], arr[j]] = [arr[j], arr[j+1]]
+            }
+        }
     }
 }
