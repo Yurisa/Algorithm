f91483ecb754e3b709c0b5e36d5aa599ae920c42
Yurisa <779509411@qq.com>
2018-04-01 19:33:59 +0800

    三路快排

--- js基础算法/sort.js
+++ js基础算法/sort.js
@@ -51,15 +51,34 @@ function insertionSort(arr){
  */
 function insertionSort2(arr){
     for(let i = 1; i < arr.length; i++){
-        var e = arr[i]
-        for(let j = i; j > 0&& arr[j-1] > e; j--){
+        let e = arr[i]
+        let j;
+        for(j = i; j > 0 && arr[j-1] > e; j--){
            arr[j] = arr[j-1]
         }
         arr[j] = e
     }
 }
 
-
+/**
+ * 对特定区间的插入排序
+ * @param {*} arr 
+ */
+/**
+ * 对特定区间的插入排序
+ * @param {*} arr 
+ */
+function insertionSort3(arr, l, r){
+    for(let i = l+1; i <= r; i++){
+        let e = arr[i]; 
+        let j;
+        for(j = i; j > l && arr[j-1] > e; j--){
+            arr[j] = arr[j-1];
+        }
+        arr[j] = e;
+    }
+    return;
+}
 /**
  * 归并排序
  * @param {*} arr 
@@ -75,8 +94,12 @@ function mergeSort(arr){
  * @param {*} r 
  */
 function __mergeSort(arr, l, r){
-    if(l >= r)
-      return;
+    // if(l >= r)
+    //   return;
+    if(r - l <= 15){
+        insertionSort3(arr, l, r)
+        return;
+    }
     let mid = parseInt((l+r)/2);
     __mergeSort(arr, l, mid);
     __mergeSort(arr, mid+1, r);
@@ -113,4 +136,126 @@ function __merge(arr, l, mid, r){
            j++;
        }
    }
+}
+
+
+/**
+ * 自底向上的归并排序
+ * @param {*} arr 
+ */
+function mergeSortBU(arr){
+    let n = arr.length;
+    for(let sz = 1; sz <= n; sz += sz){
+        for(let i = 0; i + sz < n; i += sz + sz){
+            //对arr[i...i+sz-1]和arr[i+sz...i+2*sz-1]进行归并
+            __merge(arr, i, i + sz -1, Math.min(i + sz + sz -1, n - 1))
+        }
+    }
+}
+
+
+/**
+ * 快速排序
+ * @param {*} arr 
+ */
+function quickSort(arr){
+   __quickSort(arr, 0, arr.length - 1)
+}
+
+/**
+ * 对arr[l...r]部分进行快速排序
+ * @param {*} arr 
+ * @param {*} l 
+ * @param {*} r 
+ */
+function __quickSort(arr, l, r){
+    //   if(l >= r)
+    //     return;
+     if(r - l <= 15){
+            insertionSort3(arr, l, r)
+            return;
+      }
+      let p = __partition(arr, l, r);
+      __quickSort(arr, l, p - 1);
+      __quickSort(arr, p + 1, r)
+}
+
+/**
+ * 对arr[l...r]部分进行partition操作
+ * 返回p,使得arr[l...p-1] < arr[p];arr[p+1..r] > arr[p]
+ * @param {*} arr 
+ * @param {*} l 
+ * @param {*} r 
+ */
+function __partition(arr, l, r){
+    let v = arr[l];
+
+    //arr[l+1...j] < v; arr[j+1...i] > v
+    let j = l;
+    for(let i = l+1; i <= r; i++){
+        if(arr[i] < v){
+            [arr[j+1], arr[i]] = [arr[i], arr[j+1]];
+            j++;
+        }
+    }
+    [arr[l], arr[j]] = [arr[j], arr[l]];
+    return j;
+}
+
+function __partition2(arr, l, r){
+    let v = arr[l];
+    let i = l+1; 
+    let j = r;
+    while(true){
+        while(i < r && arr[i] < v) i++;
+        while(j >= l+1 && arr[j] > v) j--;
+        if(i > j) break;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        i++;
+        j--;
+    }
+
+    [arr[l], arr[j]] = [arr[j], arr[l]]
+}
+
+
+function quickSort3Ways(arr){
+    __quickSort3Ways(arr, 0, arr.length - 1 );
+}
+
+/**
+ * 三路快速排序处理 arr[l...r]
+ * 将arr[l..r] 分为 <v ; =v; >v 三部分
+ * 之后递归对 < v ; > v两部分继续进行三路快排
+ * @param {*} arr 
+ * @param {*} l 
+ * @param {*} r 
+ */
+function __quickSort3Ways(arr, l, r){
+    if(l - r <= 15){
+        insertionSort3(arr, l, r);
+        return;
+    }
+    
+    //partition
+    let v = arr[l];
+    let lt = l; //arr[l+1...lt] < v
+    let gt = r+1;// arr[gt...r] > v
+    let i = l+1; //arr[lt+1...i] == v
+    while(i < gt){
+        if(arr[i] < v){
+            [arr[i], arr[lt+1]] = [arr[lt+1], arr[i]];
+            lt++;
+            i++;
+        }else if(arr[i] > v){
+            [arr[i], arr[gt-1]] = [arr[gt-1], arr[i]]
+            gt--;
+        }else{ //arr[i] == v
+            i++;
+        }        
+    }
+    [arr[l], arr[lt]] = [arr[lt], arr[l]];
+    __quickSort3Ways(arr, l, lt - 1);
+    __quickSort3Ways(arr, gt, r)    
+
 }
