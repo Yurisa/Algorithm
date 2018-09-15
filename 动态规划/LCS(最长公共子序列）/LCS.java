4354c7d5912d9434904247686dd4965e832427ae
Yurisa <779509411@qq.com>
2018-09-15 23:36:05 +0800

    最长公共子序列

---
+++ 动态规划/LCS(最长公共子序列）/LCS.java
@@ -0,0 +1,27 @@
+public class LCS {
+    public static int findLCS(String A, int n, String B, int m) {
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        String A = "1A2C3D4B56";
+        String B = "B1D23CA45B6A";
+        System.out.println(findLCS(A, A.length(), B, B.length()));
+    }
+}
